bdb07181a34f431f7533dd86289a6b845dd3b4f6
nima.yaghouti <nima.yaghouti@partsoftware.com>
2024-09-21 18:37:35 +0330

    feat: useClickOutside hook implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useRef } from "react";
+import useClickOutside from "./hooks/useClickOutside";
 import "./App.scss";
-function App() {
+const App = () => {
+  const testRef = useRef(null);
+
+  useClickOutside(testRef, () => {
+    console.log("outside of test element clicked!");
+  });
+
   return (
     <>
-      <div className="test">React App</div>
+      <div ref={testRef} className="test">
+        React App
+      </div>
     </>
   );
-}
+};
 
 export default App;
