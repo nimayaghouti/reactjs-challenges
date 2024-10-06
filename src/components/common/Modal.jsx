6c41a973479d22daab6ede30371adf4c503c44b8
nima.yaghouti <nima.yaghouti@partsoftware.com>
2024-10-07 00:18:38 +0330

    feat: modal improved

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 import useClickOutside from "@/hooks/useClickOutside";
 import CloseIcon from "@/assets/svg/CloseIcon";
@@ -6,17 +6,36 @@ import "./Modal.scss";
 
 const Modal = ({ isOpen, closeModal, children }) => {
   const modalRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(isOpen);
+
   useClickOutside(modalRef, closeModal);
 
+  useEffect(() => {
+    if (isOpen) {
+      setIsVisible(true);
+    }
+  }, [isOpen]);
+
+  const handleAnimationEnd = () => {
+    if (!isOpen) {
+      setIsVisible(false);
+    }
+  };
+
   return createPortal(
-    <div className={`modal ${isOpen ? "modal_open" : ""}`}>
-      <div ref={modalRef} className="modal__content">
-        <span className="modal__close-icon" onClick={closeModal}>
-          <CloseIcon />
-        </span>
-        {children}
+    isVisible && (
+      <div
+        className={`modal ${isOpen ? "modal_open" : ""}`}
+        onAnimationEnd={handleAnimationEnd}
+      >
+        <div ref={modalRef} className="modal__content">
+          <span className="modal__close-icon" onClick={closeModal}>
+            <CloseIcon />
+          </span>
+          {children}
+        </div>
       </div>
-    </div>,
+    ),
     document.getElementById("overlay")
   );
 };
